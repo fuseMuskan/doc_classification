8f8b605b34affa32f166b15ffa213a2070309fdb
fuseMuskan <muskan.bhandari@fusemachines.com>
2024-02-08 17:38:19 +0545

    function: added a function to save the trained model as a onnx model

--- utils.py
+++ utils.py
@@ -39,5 +39,22 @@ def save_model(model: torch.nn.Module, target_dir: str, model_name: str):
     ), "model_name should end with '.pt' or '.pth'"
     model_save_path = target_dir_path / model_name
 
+
+    # save onnx model
+    save_onnx_model(model_name=model_name, model=model, target_dir=target_dir)
+
+
     print(f"[INFO] Saving model to: {model_save_path}")
     torch.save(obj=model.state_dict(), f=model_save_path)
+
+def save_onnx_model(model_name:str, model:torch.nn.Module, target_dir):
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    print("[INFO] Saving model in onnx format")
+    model_name = model_name.split(".")[0] + ".onnx"
+    torch_input = torch.randn(1, 3, 224, 224, device=device)
+    onnx_program = torch.onnx.dynamo_export(model, torch_input)
+    save_path = target_dir +"/" + model_name
+    onnx_program.save(save_path)
+    print(f"[INFO] Sucessfully saved model in onnx format in {save_path}")
